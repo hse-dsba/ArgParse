cec344ce75d31f3d21357cb4e73cbc5dcc30234b
GennaFomin <genafom1n@yandex.ru>
2021-09-11 12:27:39 +0300

    Well all arguments are obligatory, and it is veryyyyyy sensitive to the way you enter your answers ( only like it is said in -help ). But it works)

--- main.py
+++ main.py
@@ -4,20 +4,40 @@ parser = argparse.ArgumentParser(description='Testing healthy lifestyle index.')
 
 indexSumm = 0
 
-parser.add_argument('Name', help = 'Your name (Optional)')
+parser.add_argument('Name', help = 'Your name (Optional). Very Important to answer directly like the answers are written!!')
 parser.add_argument('Gender', help = 'Male/Female (Optional)')
 parser.add_argument('Height', help = 'Enter your height - obligatory')
 parser.add_argument('Weight', help = "Enter your weight - obligatory")
-parser.add_argument('SleepHours', help = 'Home many hours do you sleep per day? <7/7-8/>8')
-parser.add_argument('AmmountOfMeals', help = 'Home many meals do you take a day? 1/2-3/4-5')
-parser.add_argument('AmmountFreshFruit', help = 'How many fresh fruits do you eat per day? <?> 500 g')
-parser.add_argument('Steps', help = 'How many steps do you make per day? <5k/5-10k/>10k')
-parser.add_argument('HealthMonitoring', help = 'Do you monitor your health? ')
-
-
+parser.add_argument('SleepHours', help = 'Home many hours do you sleep per day? -7/8/9')
+parser.add_argument('AmmountOfMeals', help = 'Home many meals do you take a day? 1/3/5')
+parser.add_argument('AmmountFreshFruit', help = 'How many fresh fruits do you eat per day? 300/500 ')
+parser.add_argument('Steps', help = 'How many steps do you make per day? 5/7/10')
+parser.add_argument('HealthMonitoring', help = 'Do you monitor your health? 0/1/0.5')
+parser.add_argument('Mood', help = 'Which mood? 1/0/-1')
+parser.add_argument('LastTimeHappy', help = 'Week/Month/Year')
 
 args = parser.parse_args()
 
-print(args.Name, args.Gender)
+BMI = int(args.Weight) / int((args.Height)) ** 2
+
+if BMI >= 18.5 and BMI <= 24.9:
+    indexSumm += 1
+if args.SleepHours == '8':
+    indexSumm += 1
+if args.AmmountOfMeals == '5':
+    indexSumm += 1
+if args.AmmountOfMeals == '500':
+    indexSumm += 1
+if args.Steps == '10':
+    indexSumm += 1
+if args.HealthMonitoring == '1':
+    indexSumm += 1
+if args.Mood == '1':
+    indexSumm += 1
+if args.LastTimeHappy == 'Week':
+    indexSumm += 1
+
+
+print('Yours healthy lifestyle Index is', indexSumm)
 
 
